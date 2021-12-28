4300477da961d12a3e006a866c2e437caa2e0bb9
Dean <dean@deansantillan.com>
2021-12-28 14:31:52 +1000

    task(): added css and code for handling most buttons

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
+const calcDisplay = document.querySelector('h1');
+const inputBtns = document.querySelectorAll('button');
+const clearBtn = document.getElementById('clear-btn');
 
+const sendNumberValue = (number) => () => {
+	const displayValue = calcDisplay.textContent;
+	calcDisplay.textContent =
+		displayValue === '0' ? number : displayValue + number;
+};
+
+const addDecimal = () => {
+	if (!calcDisplay.textContent.includes('.')) {
+		calcDisplay.textContent = calcDisplay.textContent + '.';
+	}
+};
+
+console.log(inputBtns);
+
+inputBtns.forEach((btn) => {
+	console.log(btn);
+	console.log(btn.value);
+
+	if (btn.classList.length === 0) {
+		btn.addEventListener('click', sendNumberValue(btn.value));
+	} else if (btn.classList.contains('operator')) {
+		btn.addEventListener('click', sendNumberValue(btn.value));
+	} else if (btn.classList.contains('decimal')) {
+		btn.addEventListener('click', addDecimal);
+	}
+});
+
+// reset display
+
+const resetAll = () => (calcDisplay.textContent = '0');
+clearBtn.addEventListener('click', resetAll);
