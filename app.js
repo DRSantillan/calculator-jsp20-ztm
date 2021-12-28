a893a1c127b1fce67b0cb028cc2a7a26ab91781c
Dean <dean@deansantillan.com>
2021-12-28 15:04:34 +1000

    task(): added more code.

--- app.js
+++ app.js
@@ -2,28 +2,49 @@ const calcDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 
+let firstValue = 0;
+let operatorValue = '';
+let awaitingNextValue = false;
+
 const sendNumberValue = (number) => () => {
-	const displayValue = calcDisplay.textContent;
-	calcDisplay.textContent =
-		displayValue === '0' ? number : displayValue + number;
+	if (awaitingNextValue) {
+		calcDisplay.textContent = number;
+		awaitingNextValue = false;
+	} else {
+		const displayValue = calcDisplay.textContent;
+		calcDisplay.textContent =
+			displayValue === '0' ? number : displayValue + number;
+	}
 };
 
 const addDecimal = () => {
+	if (awaitingNextValue) return;
 	if (!calcDisplay.textContent.includes('.')) {
 		calcDisplay.textContent = calcDisplay.textContent + '.';
 	}
 };
 
-console.log(inputBtns);
+const useOperator = (operator) => () => {
+	const currentValue = Number(calcDisplay.textContent);
+    // prevent multiple operator pushes
+    if(operatorValue && awaitingNextValue) return;
+	if (!firstValue) {
+		firstValue = currentValue;
+	} else {
+		console.log(firstValue, operatorValue, currentValue);
+	}
+	//
+	awaitingNextValue = true;
+	operatorValue = operator;
+};
+
 
 inputBtns.forEach((btn) => {
-	console.log(btn);
-	console.log(btn.value);
 
 	if (btn.classList.length === 0) {
 		btn.addEventListener('click', sendNumberValue(btn.value));
 	} else if (btn.classList.contains('operator')) {
-		btn.addEventListener('click', sendNumberValue(btn.value));
+		btn.addEventListener('click', useOperator(btn.value));
 	} else if (btn.classList.contains('decimal')) {
 		btn.addEventListener('click', addDecimal);
 	}
@@ -31,5 +52,10 @@ inputBtns.forEach((btn) => {
 
 // reset display
 
-const resetAll = () => (calcDisplay.textContent = '0');
+const resetAll = () => {
+	firstValue = 0;
+	operatorValue = '';
+	awaitingNextValue = false;
+	calcDisplay.textContent = '0';
+};
 clearBtn.addEventListener('click', resetAll);
